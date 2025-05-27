9d9062c4a82532ad2caef54ecfd4ebeb46eb5450
lexvvcy <alexcihc@gmail.com>
2025-05-27 19:28:27 +0800

    test

--- src/RISCV_Pipeline.v
+++ src/RISCV_Pipeline.v
@@ -3,7 +3,7 @@ module ALU(
     input  [3:0]        alu_ctrl,    // 0=ADD,1=SUB,2=AND,3=OR,4=XOR,5=SLL,6=SRL,7=SRA,8=SLT
     input  signed [31:0] data1,
     input  signed [31:0] data2,
-    output signed [31:0] alu_calc,
+    output signed [31:0] alu_calc
 );  
 
     // I think we should not use zero, because zero is in the comparator, and the branch logic is in ID/EX
@@ -65,13 +65,76 @@ reg [31:0] PC_w;
 reg [31:0] PC_reg;
 reg        IF_valid_w;
 reg [31:0] IF_pc_w,IF_pc_r;
-reg [31:0] IIF_inst_w,F_inst_r;
+reg [31:0] IF_inst_w,IF_inst_r;
 
+assign PC = PC_reg; // Output current PC value
 assign ICACHE_ren = 1'b1;
 assign ICACHE_wen = 1'b0; 
 assign ICACHE_addr = PC_reg[31:2];
 assign ICACHE_wdata = 32'd0; 
 
+//ID -> Branch here ...
+
+// wire :)
+reg    jal;
+reg    jalr;
+reg    branch;
+reg    ALU_src;
+reg    Reg_write;
+reg    mem_to_reg;
+reg    mem_wen_D;
+reg    bne;
+reg    [31:0] immediate;
+reg    [3:0] alu_ctrl;
+
+reg [31:0] ID_rs1_w,ID_rs1_r, ID_rs2_w,ID_rs2_r,ID_imm_w,ID_imm_r;
+reg [4:0]  ID_rs1_addr_w,ID_rs1_addr_r,ID_rs2_addr_w,ID_rs2_addr_r,ID_rd_w,ID_rd_r;
+reg        ID_mem_to_reg_w,ID_mem_to_reg_r, ID_mem_wen_D_w,ID_mem_wen_D_r, ID_Reg_write_w,ID_Reg_write_r, ID_ALU_src_w,ID_ALU_src_r;
+reg [3:0]  ID_alu_ctrl_w,ID_alu_ctrl_r;
+
+// Hazard detection
+wire hazard;
+reg [1:0] cnt_w, cnt_r;
+
+//zero
+wire zero;
+
+// Branch target calculation
+reg [31:0] branch_jal_addr;
+reg [31:0] jalr_addr;
+
+//EX
+wire [31:0] rs1_val, rs2_val,EX_op1, EX_op2;
+reg  [31:0] EX_out_w, EX_out_r;
+reg  [31:0] EX_rs2_w, EX_rs2_r;
+reg   [4:0] EX_rd_w, EX_rd_r;
+reg         EX_mem_to_reg_w,EX_mem_to_reg_r, EX_mem_wen_D_w, EX_mem_wen_D_r,EX_Reg_write_w, EX_Reg_write_r;
+wire [31:0] alu_result;
+
+// Forwarding logic
+reg [1:0] forwardA, forwardB;
+
+//MEM
+wire [31:0] MEM_alu_out;
+assign MEM_alu_out = EX_out_r;
+
+wire [31:0] MEM_wdata;
+assign MEM_wdata = EX_rs2_r; // Data to write to memory
+
+reg [31:0] MEM_rdata_w, MEM_rdata_r;
+reg [4:0]  MEM_rd_w, MEM_rd_r;
+reg        MEM_mem_to_reg_w, MEM_mem_to_reg_r,MEM_Reg_write_w, MEM_Reg_write_r;
+
+//WB
+wire [31:0] WB_alu_out;
+assign WB_alu_out = (MEM_mem_to_reg_r)? MEM_rdata_r : MEM_alu_out; // Choose between memory read data and ALU output
+
+// Register file
+reg [31:0] RF_r [0:31]; 
+
+
+
+////////////////////////// IF Stage //////////////////////////
 always@(*) begin
     PC_w = PC_reg + 32'd4; // Increment PC by 4 for next instruction
     IF_valid_w = 1'b1; // Set IF stage valid flag
@@ -121,28 +184,7 @@ always@(posedge clk) begin
     end
 end
 
-//ID -> Branch here ...
-
-// wire :)
-reg    jal;
-reg    jalr;
-reg    branch;
-reg    ALU_src;
-reg    Reg_write;
-reg    mem_to_reg;
-reg    mem_wen_D;
-reg    bne;
-reg    [31:0] immediate;
-reg    [3:0] alu_ctrl;
-
-reg [31:0] ID_rs1_w,ID_rs1_r, ID_rs2_w,ID_rs2_r,ID_imm_w,ID_imm_r;
-reg [4:0]  ID_rs1_addr_w,ID_rs1_addr_r,ID_rs2_addr_w,ID_rs2_addr_r,ID_rd_w,ID_rd_r;
-reg        ID_mem_to_reg_w,ID_mem_to_reg_r, ID_mem_wen_D_w,ID_mem_wen_D_r, ID_Reg_write_w,ID_Reg_write_r, ID_ALU_src_w,ID_ALU_src_r;
-reg [3:0]  ID_alu_ctrl_w,ID_alu_ctrl_r;
-
-// Hazard detection
-wire hazard;
-reg [1:0] cnt_w, cnt_r;
+////////////////////////// ID Stage //////////////////////////
 
 always@(*) begin
     cnt_w = cnt_r; // Default to hold the counter value
@@ -244,7 +286,7 @@ always@(*) begin
 
     // 0=ADD,1=SUB,2=AND,3=OR,4=XOR,5=SLL,6=SRL,7=SRA,8=SLT
 
-    case(inst[6:0])  // R: add,sub,and,or,xor,slli,srai,srli,slt  I:addi,andi,ori,xori,slti,lw,jalr  S:sw  B:beq,bne  J:jal
+    case(IF_inst_r[6:0])  // R: add,sub,and,or,xor,slli,srai,srli,slt  I:addi,andi,ori,xori,slti,lw,jalr  S:sw  B:beq,bne  J:jal
     
         // R: add,sub,and,or,xor,slt
         7'b0110011: begin   
@@ -307,28 +349,19 @@ always@(*) begin
     endcase
 end
 
-wire zero;
 comparator cpr(
     .data1(ID_rs1_w),
     .data2(ID_rs2_w),
     .zero(zero)
 );
 
-// Branch target calculation
-reg [31:0] branch_jal_addr;
-reg [31:0] jalr_addr;
+// Branch address calculation
 always@(*) begin
     branch_jal_addr = IF_pc_r + immediate;
     jalr_addr = ID_rs1_w + immediate;
 end
 
-//EX
-wire [31:0] rs1_val, rs2_val,EX_op1, EX_op2;
-reg  [31:0] EX_out_w, EX_out_r;
-reg  [31:0] EX_rs2_w, EX_rs2_r;
-reg   [4:0] EX_rd_w, EX_rd_r;
-reg         EX_mem_to_reg_w,EX_mem_to_reg_r, EX_mem_wen_D_w, EX_mem_wen_D_r,EX_Reg_write_w, EX_Reg_write_r;
-wire [31:0] alu_result;
+////////////////////////// EX Stage //////////////////////////
 
 // instantiate ALU
 ALU alu_u(
@@ -338,18 +371,17 @@ ALU alu_u(
     .alu_calc(alu_result)
 );
 
-// Forwarding logic
-reg [1:0] forwardA, forwardB;
+
 always @(*) begin
     forwardA = 2'b00;
     forwardB = 2'b00;
-    if (MEM_reg_wen && MEM_rd!=5'd0) begin
-        if (MEM_rd==ID_rs1_addr_r) forwardA = 2'b10;
-        if (MEM_rd==ID_rs2_addr_r) forwardB = 2'b10;
+    if (EX_Reg_write_r && EX_rd_r!=5'd0) begin
+        if (EX_rd_r==ID_rs1_addr_r) forwardA = 2'b10;
+        if (EX_rd_r==ID_rs2_addr_r) forwardB = 2'b10;
     end
-    if (WB_reg_wen && WB_rd!=5'd0) begin
-        if (WB_rd==ID_rs1_addr_r && forwardA==2'b00) forwardA=2'b01;
-        if (WB_rd==ID_rs2_addr_r && forwardB==2'b00) forwardB=2'b01;
+    if (MEM_Reg_write_r && MEM_rd_r!=5'd0) begin
+        if (MEM_rd_r==ID_rs1_addr_r && forwardA==2'b00) forwardA=2'b01;
+        if (MEM_rd_r==ID_rs2_addr_r && forwardB==2'b00) forwardB=2'b01;
     end
 end
 
@@ -403,16 +435,7 @@ always@(*) begin
     end
 end
 
-//MEM
-wire [31:0] MEM_alu_out;
-assign MEM_alu_out = EX_out_r;
-
-wire [31:0] MEM_wdata;
-assign MEM_wdata = EX_rs2_r; // Data to write to memory
-
-reg [31:0] MEM_rdata_w, MEM_rdata_r;
-reg [4:0]  MEM_rd_w, MEM_rd_r;
-reg        MEM_mem_to_reg_w, MEM_mem_to_reg_r,MEM_Reg_write_w, MEM_Reg_write_r;
+////////////////////////// MEM Stage //////////////////////////
 
 assign DCACHE_ren = !EX_mem_wen_D_r; // Read enable for D-cache
 assign DCACHE_wen = EX_mem_wen_D_r; // Write enable for D-cache
@@ -421,7 +444,7 @@ assign DCACHE_wdata = {MEM_wdata[7:0],MEM_wdata[15:8],MEM_wdata[23:16],MEM_wdata
 
 always@(*) begin
     MEM_rdata_w = {DCACHE_rdata[7:0],DCACHE_rdata[15:8],DCACHE_rdata[23:16],DCACHE_rdata[31:24]}; // Data read from D-cache
-    MEM_rd = EX_rd_r; // Forward rd from EX stage
+    MEM_rd_w = EX_rd_r; // Forward rd from EX stage
     MEM_mem_to_reg_w = EX_mem_to_reg_r; // Forward mem_to_reg flag from EX stage
     MEM_Reg_write_w = EX_Reg_write_r; // Forward Reg write flag from EX stage
 
@@ -451,17 +474,11 @@ always@(posedge clk) begin
 
 end
 
-//WB
-wire [31:0] WB_alu_out;
-assign WB_alu_out = (MEM_mem_to_reg_r)? MEM_rdata_r : MEM_alu_out; // Choose between memory read data and ALU output
-
-// Register file
-reg [31:0] RF_r [0:31]; 
-
+////////////////////////// WB Stage //////////////////////////
+integer i;
 always@(posedge clk) begin
     if (!rst_n) begin
         // Reset register file to zero
-        integer i;
         for (i = 0; i < 32; i = i + 1) begin
             RF_r[i] <= 32'd0;
         end

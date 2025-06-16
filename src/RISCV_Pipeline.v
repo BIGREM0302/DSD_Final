6e70c906b18677c5160f4734521827605519b7e4
lexvvcy <alexcihc@gmail.com>
2025-06-16 17:17:46 +0800

    *

--- src/RISCV_Pipeline.v
+++ src/RISCV_Pipeline.v
@@ -78,344 +78,6 @@ module PredictionUnit (
 
 endmodule
 
-module BoothMul (
-    clk,
-    rst_n,
-    stall,
-    a,
-    b,
-    m
-);
-
-
-input wire        clk, rst_n, stall;
-input wire [31:0] a, b; //a be multiplicand, b be multiplier
-output     [31:0] m; // m = a * b (unsigned)
-
-reg [33:0] PProd [0:15];
-reg [63:0] shifted [0:15];
-
-reg [63:0] temp1 [0:11];
-reg [63:0] temp2 [0:7];
-reg [63:0] temp3 [0:5];
-reg [63:0] temp4 [0:3];
-reg [63:0] temp5 [0:2];
-reg [63:0] temp6 [0:1];
-
-// we will test EX : stage1 , MEM: stage 2->3 , WB: stage 4->5->6
-// so that we need to mannually give "hazard" for those who can't use forwarding to rescue
-
-reg [63:0] temp2_w [0:7];
-reg [63:0] temp2_r [0:7];
-wire [31:0] m_temp;
-reg [31:0] m_w, m_r;
-wire [56:0] upper_sum = temp6[0][63:8] + temp6[1][63:8];
-assign m_temp = {upper_sum[23:0], temp6[0][7:0]};
-assign m = m_r;
-reg overflow;
-
-////////////////////////////////// Booth Encoding //////////////////////////////////
-
-//partial product generation
-function [33:0] cal_PProd;
-    input [2:0] mul;
-    input [32:0] M; //M should include sign bit
-    reg [33:0] MX0, MX1, MX2, MX_2, MX_1;
-begin
-
-    MX0 = 34'd0;
-    MX1 = {1'b0, M};
-    MX_1 = ~{1'b0, M}+1'b1; //need to reconsider this
-    MX2 = {M, 1'b0};
-    MX_2 = ~{M, 1'b0}+1'b1; //need to reconsider this
-
-    case(mul)
-        3'd0, 3'd7: cal_PProd = MX0;
-        3'd1, 3'd2: cal_PProd = MX1;
-        3'd3: cal_PProd = MX2;
-        3'd4: cal_PProd = MX_2;
-        3'd5, 3'd6: cal_PProd = MX_1;
-        default: cal_PProd = MX0;
-    endcase
-end
-endfunction
-
-function [1:0] HA_compress;
-    input a;
-    input b;
-    reg [1:0] result;
-begin
-    result[1] = a & b;
-    result[0] = a ^ b;
-    HA_compress = result;
-end
-endfunction
-
-function [1:0] FA_compress;
-    input a;
-    input b;
-    input cin;
-    reg [1:0] result;
-begin
-    result[1] = (a&b) | (b&cin) | (a&cin); // cout
-    result[0] = (a) ^ (b) ^ (cin);
-    FA_compress = result;
-end
-endfunction
-
-integer i, j;
-
-always@(*) begin
-    for( i = 0; i < 16; i = i + 1) begin
-        PProd[i] = cal_PProd({b[2*i+1], b[2*i], (i==0 ? 1'b0:b[2*i-1])}, {1'b0, a});
-        shifted[i] = {{30{PProd[i][33]}}, PProd[i]} << (2*i);
-    end
-
-// Tree
-
-// ================================== stage 1 ==========================================================
-    for (i = 0; i < 11; i = i + 1)begin
-        //initialize
-        temp1[i] = 64'd0;
-    end
-
-    for(j = 0; j <= 4; j = j + 1) begin        
-        for(i = j*6; i < 64; i = i + 1) begin
-            if( i == j*6 || i == (j*6+1))begin
-                temp1[2*j][i] = shifted[3*j][i];
-            end
-            else if(i == (j*6+2) || i == (j*6+3)) begin
-                {temp1[2*j+1][i+1], temp1[2*j][i]} = HA_compress(shifted[3*j][i], shifted[3*j+1][i]);
-            end
-            else if (i == 63) begin
-                {overflow, temp1[(2*j)][i]} = FA_compress(shifted[3*j][i], shifted[3*j+1][i], shifted[3*j+2][i]);
-            end
-            else begin
-                {temp1[(2*j+1)][i+1], temp1[(2*j)][i]} = FA_compress(shifted[3*j][i], shifted[3*j+1][i], shifted[3*j+2][i]);
-            end
-        end
-    end
-
-    temp1[10] = shifted[15];
-
-// ================================== stage 2 ==========================================================
-    for (i = 0; i < 8; i = i + 1)begin
-        //initialize
-        temp2[i] = 64'd0;
-    end
-    // for 0, 1
-    j = 0;
-    for(i = 0; i < 64; i = i + 1) begin
-        if( i < 3 )begin
-            temp2[2*j][i] = temp1[3*j][i];
-        end
-        else if(i >= 3 && i < 6) begin
-            {temp2[2*j+1][i+1], temp2[2*j][i]} = HA_compress(temp1[3*j][i], temp1[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-        else begin
-            {temp2[(2*j+1)][i+1], temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-    end
-    j = 1;
-    // for 2, 3
-    for(i = 9; i < 64; i = i + 1) begin
-        if( i < 12 )begin
-            temp2[2*j][i] = temp1[3*j][i];
-        end
-        else if(i >= 12 && i < 15) begin
-            {temp2[2*j+1][i+1], temp2[2*j][i]} = HA_compress(temp1[3*j][i], temp1[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-        else begin
-            {temp2[(2*j+1)][i+1], temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-    end
-    j = 2;
-    // for 4, 5
-    for(i = 18; i < 64; i = i + 1) begin
-        if( i < 21 )begin
-            temp2[2*j][i] = temp1[3*j][i];
-        end
-        else if(i >= 21 && i < 24) begin
-            {temp2[2*j+1][i+1], temp2[2*j][i]} = HA_compress(temp1[3*j][i], temp1[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-        else begin
-            {temp2[(2*j+1)][i+1], temp2[(2*j)][i]} = FA_compress(temp1[3*j][i], temp1[3*j+1][i], temp1[3*j+2][i]);
-        end
-    end
-
-    // for 6, 7
-    temp2[6] = temp1[9];
-    temp2[7] = temp1[10];
-
-// ================================== stage 3 ==========================================================
-    for (i = 0; i < 6; i = i + 1)begin
-        //initialize
-        temp3[i] = 64'd0;
-    end
-    // for 0, 1
-    j = 0;
-    for(i = 0; i < 64; i = i + 1) begin
-        if( i < 4 )begin
-            temp3[2*j][i] = temp2_r[3*j][i];
-        end
-        else if(i >= 4 && i < 9) begin
-            {temp3[2*j+1][i+1], temp3[2*j][i]} = HA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp3[(2*j)][i]} = FA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i], temp2_r[3*j+2][i]);
-        end
-        else begin
-            {temp3[(2*j+1)][i+1], temp3[(2*j)][i]} = FA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i], temp2_r[3*j+2][i]);
-        end
-    end
-    // for 2, 3
-    j = 1;
-    for(i = 13; i < 64; i = i + 1) begin
-        if( i < 18 )begin
-            temp3[2*j][i] = temp2_r[3*j][i];
-        end
-        else if(i >= 18 && i < 22) begin
-            {temp3[2*j+1][i+1], temp3[2*j][i]} = HA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp3[(2*j)][i]} = FA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i], temp2_r[3*j+2][i]);
-        end
-        else begin
-            {temp3[(2*j+1)][i+1], temp3[(2*j)][i]} = FA_compress(temp2_r[3*j][i], temp2_r[3*j+1][i], temp2_r[3*j+2][i]);
-        end
-    end
-    // for 4, 5
-    temp3[4] = temp2_r[6];
-    temp3[5] = temp2_r[7];
-
-// ================================== stage 4 ==========================================================
-    for (i = 0; i < 4; i = i + 1)begin
-        //initialize
-        temp4[i] = 64'd0;
-    end
-    // for 0, 1
-    j = 0;
-    for(i = 0; i < 64; i = i + 1) begin
-        if( i < 5 )begin
-            temp4[2*j][i] = temp3[3*j][i];
-        end
-        else if(i >= 5 && i < 13) begin
-            {temp4[2*j+1][i+1], temp4[2*j][i]} = HA_compress(temp3[3*j][i], temp3[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp4[(2*j)][i]} = FA_compress(temp3[3*j][i], temp3[3*j+1][i], temp3[3*j+2][i]);
-        end
-        else begin
-            {temp4[(2*j+1)][i+1], temp4[(2*j)][i]} = FA_compress(temp3[3*j][i], temp3[3*j+1][i], temp3[3*j+2][i]);
-        end
-    end
-    // for 2, 3
-    j = 1;
-    for(i = 19; i < 64; i = i + 1) begin
-        if( i < 27 )begin
-            temp4[2*j][i] = temp3[3*j][i];
-        end
-        else if(i >= 27 && i < 30) begin
-            {temp4[2*j+1][i+1], temp4[2*j][i]} = HA_compress(temp3[3*j][i], temp3[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp4[(2*j)][i]} = FA_compress(temp3[3*j][i], temp3[3*j+1][i], temp3[3*j+2][i]);
-        end
-        else begin
-            {temp4[(2*j+1)][i+1], temp4[(2*j)][i]} = FA_compress(temp3[3*j][i], temp3[3*j+1][i], temp3[3*j+2][i]);
-        end
-    end
-
-// ================================== stage 5 ==========================================================
-    for (i = 0; i < 3; i = i + 1)begin
-        //initialize
-        temp5[i] = 64'd0;
-    end
-    // for 0, 1
-    j = 0;
-    for(i = 0; i < 64; i = i + 1) begin
-        if( i < 6 )begin
-            temp5[2*j][i] = temp4[3*j][i];
-        end
-        else if(i >= 6 && i < 19) begin
-            {temp5[2*j+1][i+1], temp5[2*j][i]} = HA_compress(temp4[3*j][i], temp4[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp5[(2*j)][i]} = FA_compress(temp4[3*j][i], temp4[3*j+1][i], temp4[3*j+2][i]);
-        end
-        else begin
-            {temp5[(2*j+1)][i+1], temp5[(2*j)][i]} = FA_compress(temp4[3*j][i], temp4[3*j+1][i], temp4[3*j+2][i]);
-        end
-    end
-    // for 2
-    temp5[2] = temp4[3];
-
-// ================================== stage 6 ==========================================================
-    for (i = 0; i < 2; i = i + 1)begin
-        //initialize
-        temp6[i] = 64'd0;
-    end
-    j = 0;
-    for(i = 0; i < 64; i = i + 1) begin
-        if( i < 7 )begin
-            temp6[2*j][i] = temp5[3*j][i];
-        end
-        else if(i >= 7 && i < 28) begin
-            {temp6[2*j+1][i+1], temp6[2*j][i]} = HA_compress(temp5[3*j][i], temp5[3*j+1][i]);
-        end
-        else if (i == 63) begin
-            {overflow, temp6[(2*j)][i]} = FA_compress(temp5[3*j][i], temp5[3*j+1][i], temp5[3*j+2][i]);
-        end
-        else begin
-            {temp6[(2*j+1)][i+1], temp6[(2*j)][i]} = FA_compress(temp5[3*j][i], temp5[3*j+1][i], temp5[3*j+2][i]);
-        end
-    end
-end
-
-always@(*) begin
-    if(stall) begin
-        for (i = 0; i < 8; i = i + 1) begin
-            temp2_w[i] = temp2_r[i];
-        end
-        m_w = m_r;
-    end
-
-    else begin
-        for (i = 0; i < 8; i = i + 1) begin
-            temp2_w[i] = temp2[i];
-        end
-        m_w = m_temp;
-    end
-end
-
-always@(posedge clk) begin
-
-    if(!rst_n) begin
-        for (i = 0; i < 8; i = i + 1) begin
-            temp2_r[i] <= 64'd0;
-        end
-        m_r <= 32'd0;
-    end 
-
-    else begin
-        for (i = 0; i < 8; i = i + 1) begin
-            temp2_r[i] <= temp2_w[i];
-        end
-        m_r <= m_w;
-    end
-end
-
-endmodule
-
 module decompressor (
     input  wire [15:0] c,
     output reg  [31:0] r
@@ -636,6 +298,7 @@ wire       hazard;
 
 // ALU source and execution
 wire        [31:0] alu_result;
+wire        [31:0] ALU_mul;
 wire        [31:0] rs1_val, rs2_val,EX_op1, EX_op2;
 reg  signed [31:0] EX_out_w, EX_out_r;
 reg         [31:0] EX_rs2_w, EX_rs2_r;
@@ -643,7 +306,6 @@ reg         [4:0 ] EX_rd_w, EX_rd_r;
 reg                EX_mem_to_reg_w,EX_mem_to_reg_r;
 reg                EX_mem_wen_D_w, EX_mem_wen_D_r;
 reg                EX_Reg_write_w, EX_Reg_write_r;
-reg                EX_mul_w, EX_mul_r;
 
 // Forwarding logic
 reg [1:0] forwardA, forwardB;
@@ -660,12 +322,10 @@ reg [31:0] MEM_rdata_w, MEM_rdata_r;
 reg [4:0 ] MEM_rd_w, MEM_rd_r;
 reg        MEM_mem_to_reg_w, MEM_mem_to_reg_r;
 reg        MEM_Reg_write_w, MEM_Reg_write_r;
-reg        MEM_mul_w, MEM_mul_r;
 
 /////////////////////////////////////////// WB Stage Variable //////////////////////////////////////////
 
 // Write-back 
-wire signed [31:0] Booth_mul;
 wire        [31:0] WB_out_w;
 
 // Register file
@@ -697,15 +357,14 @@ assign ID_B                  = IF_B_r;
 
 // we need to debug this !!! branch / load hazard
 assign ID_pc_w = (IF_C_r)?$signed(IF_pc_plus_four_r) - $signed(32'd2):$signed(IF_pc_plus_four_r) - $signed(32'd4);
-assign ID_rs1_br             = (IF_inst_r[19:15] == EX_rd_r  && EX_Reg_write_r  && EX_rd_r  != 5'd0 && EX_mul_r != 1'b1)? EX_out_r :
+assign ID_rs1_br             = (IF_inst_r[19:15] == EX_rd_r  && EX_Reg_write_r  && EX_rd_r  != 5'd0)? EX_out_r :
                                (IF_inst_r[19:15] == MEM_rd_r && MEM_Reg_write_r && MEM_rd_r != 5'd0)? WB_out_w : RF_r[{IF_inst_r[19:15]}];
-assign ID_rs2_br             = (IF_inst_r[24:20] == EX_rd_r  && EX_Reg_write_r  && EX_rd_r  != 5'd0 && EX_mul_r != 1'b1)? EX_out_r :
+assign ID_rs2_br             = (IF_inst_r[24:20] == EX_rd_r  && EX_Reg_write_r  && EX_rd_r  != 5'd0)? EX_out_r :
                                (IF_inst_r[24:20] == MEM_rd_r && MEM_Reg_write_r && MEM_rd_r != 5'd0)? WB_out_w : RF_r[{IF_inst_r[24:20]}];
-assign hazard = (ID_mem_to_reg_r | ID_mul_r | ID_B ) && ((ID_rs1_addr_w == ID_rd_r) || (ID_rs2_addr_w == ID_rd_r)) && (ID_rd_r != 5'd0);
+assign hazard = (ID_mem_to_reg_r | ID_B ) && ((ID_rs1_addr_w == ID_rd_r) || (ID_rs2_addr_w == ID_rd_r)) && (ID_rd_r != 5'd0);
 assign jump_addr = alu_result;
 assign MEM_wdata = EX_rs2_r; // SW use rs2
-assign WB_out_w = (MEM_mem_to_reg_r)? MEM_rdata_r : 
-                  (MEM_mul_r)? Booth_mul : MEM_alu_out_r; // Choose between memory read data and ALU output
+assign WB_out_w = (MEM_mem_to_reg_r)? MEM_rdata_r : MEM_alu_out_r; // Choose between memory read data and ALU output
 
 
 ////////////////////////// IF Stage //////////////////////////
@@ -1033,20 +692,11 @@ ALU alu_u(
     .alu_calc(alu_result)
 );
 
-BoothMul mul_u(
-    .clk(clk),
-    .rst_n(RST_n),
-    .stall(stall),
-    .a(EX_op1),
-    .b(EX_op2),
-    .m(Booth_mul)
-);
-
 // Forwarding Part !!!
 always @(*) begin
     forwardA = 2'b00;
     forwardB = 2'b00;
-    if (EX_Reg_write_r && EX_rd_r!=5'd0 && EX_mul_r != 1'b1) begin
+    if (EX_Reg_write_r && EX_rd_r!=5'd0) begin
         if (EX_rd_r==ID_rs1_addr_r) forwardA = 2'b10;
         if (EX_rd_r==ID_rs2_addr_r) forwardB = 2'b10;
     end
@@ -1067,14 +717,15 @@ assign rs2_val = (forwardB==2'b00) ? ID_rs2_r :
 // Get correct ALU operands
 assign EX_op1 = rs1_val; 
 assign EX_op2 = (ID_ALU_src_r) ? ID_imm_r : rs2_val; 
+assign ALU_mul = EX_op1 * EX_op2;
 
 always@(*) begin
     EX_rd_w = ID_rd_r; 
     EX_mem_to_reg_w = ID_mem_to_reg_r; 
     EX_mem_wen_D_w = ID_mem_wen_D_r; 
     EX_Reg_write_w = ID_Reg_write_r;
-    EX_mul_w = ID_mul_r;
-    EX_out_w = (ID_jump_r)? ID_pc_plus_four_r : alu_result; // jump address (PC+4) or other ALU result
+    EX_out_w = (ID_jump_r)? ID_pc_plus_four_r: 
+               (ID_mul_r)? ALU_mul : alu_result; // jump address (PC+4) or other ALU result
     EX_rs2_w = rs2_val; // Forward rs2 value, need to use it for SW
 
     if(stall) begin
@@ -1082,7 +733,6 @@ always@(*) begin
         EX_mem_to_reg_w = EX_mem_to_reg_r;
         EX_mem_wen_D_w = EX_mem_wen_D_r;
         EX_Reg_write_w = EX_Reg_write_r;
-        EX_mul_w = EX_mul_r;
         EX_out_w = EX_out_r;
         EX_rs2_w = EX_rs2_r; 
     end
@@ -1094,7 +744,6 @@ always@(posedge clk) begin
         EX_mem_to_reg_r <= 1'b0;
         EX_mem_wen_D_r <= 1'b0;
         EX_Reg_write_r <= 1'b0;
-        EX_mul_r <= 1'b0;
         EX_out_r <= 32'd0;
         EX_rs2_r <= 32'd0; 
     end 
@@ -1104,7 +753,6 @@ always@(posedge clk) begin
         EX_mem_to_reg_r <= EX_mem_to_reg_w; 
         EX_mem_wen_D_r <= EX_mem_wen_D_w; 
         EX_Reg_write_r <= EX_Reg_write_w; 
-        EX_mul_r <= EX_mul_w;
         EX_out_r <= EX_out_w; 
         EX_rs2_r <= EX_rs2_w; 
     end
@@ -1123,7 +771,6 @@ always@(*) begin
     MEM_rd_w = EX_rd_r; 
     MEM_mem_to_reg_w = EX_mem_to_reg_r; 
     MEM_Reg_write_w = EX_Reg_write_r; 
-    MEM_mul_w = EX_mul_r;
 
     if(stall) begin
         MEM_alu_out_w = MEM_alu_out_r;
@@ -1131,7 +778,6 @@ always@(*) begin
         MEM_rd_w = MEM_rd_r;
         MEM_mem_to_reg_w = MEM_mem_to_reg_r;
         MEM_Reg_write_w = MEM_Reg_write_r; 
-        MEM_mul_w = MEM_mul_r;
     end
 end
 
@@ -1142,7 +788,6 @@ always@(posedge clk) begin
         MEM_rd_r <= 5'd0;
         MEM_mem_to_reg_r <= 1'b0; 
         MEM_Reg_write_r <= 1'b0; 
-        MEM_mul_r <= 1'b0;
     end 
     
     else begin
@@ -1151,7 +796,6 @@ always@(posedge clk) begin
         MEM_rd_r <= MEM_rd_w; 
         MEM_mem_to_reg_r <= MEM_mem_to_reg_w; 
         MEM_Reg_write_r <= MEM_Reg_write_w; 
-        MEM_mul_r <= MEM_mul_w;
     end
 end
 
